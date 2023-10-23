0a5a1c1a8b47867ef13d96e797d95d8141fc9b30
Daniel Kiska <daniel.kiska@gmail.com>
2023-10-23 16:02:28 +0100

    finished second game mode

--- Program.cs
+++ Program.cs
@@ -35,18 +35,18 @@ internal class Program
                 Console.WriteLine("Invalid input choice. Please restart and select option 1 or 2 on your keyboard.");
             }
         }
-        //Catching Possible errors in the main loop.
+        //Catching Possible errors in the main loop when parsing input.
         catch (FormatException)
         {
-            Console.WriteLine("Parsing failed. Input is not a valid integer.");
+            Console.WriteLine("Error: Parsing failed. Input is not a valid integer.");
         }
         catch (ArgumentNullException)
         {
-            Console.WriteLine("Parsing failed. Input is null.");
+            Console.WriteLine("Error: Parsing failed. Input is null.");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            Console.WriteLine("Error: An unexpected error occurred: " + ex.Message);
         }
     }
 
@@ -65,14 +65,14 @@ internal class Program
         //For binary search, the total iterations required to find a number would be atmost log2(total_array_size)
         //log2(10 000) = 13.29, so computer should always guess our number in maximum of 14 attempts
 
-        //using do/while loop becouse i want to first block be executed atleast once.
+        //using do/while loop becouse I want to first block be executed atleast once.
         do
         {
             //Guessing the number.
             guess = (min + max) / 2;
             Console.WriteLine("Computer guess is: [" + guess + "], please let computer know if his guesed number is:");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("(1) Too low, (2) Too high, (3) You queesed it right!");
+            Console.WriteLine("(1) Too low, (2) Too high, (3) Correct!");
             Console.ForegroundColor = ConsoleColor.Gray;
             userResponse = Console.ReadLine();
 
@@ -87,7 +87,7 @@ internal class Program
             }
             else if (userResponse != "3")
             {
-                Console.WriteLine("Invalid user response. Please enter number 1, 2, or 3.");
+                Console.WriteLine("Error: Invalid user response. Please enter number 1, 2, or 3.");
             }
         } while (userResponse != "3");
 
@@ -100,7 +100,63 @@ internal class Program
     //Function for User guessing Computer number game using binary serach.
     static void HumanGuessNumber()
     {
-        Console.WriteLine("your turn");
-    }
+        //Generating computer choice.
+        Random random = new Random();
+        int computerChoice = random.Next(1, 10000); // Dont know what do you ment by keeping it secret? (not showing to the user?)
+        Console.WriteLine(computerChoice); //For debug only
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Computer generated a random number between 1 and 10 000 try to guess it!");
+        Console.ForegroundColor = ConsoleColor.Gray;
 
+        //Handling user guessing
+        int userGuess = 0;
+
+        do
+        {
+            //Error handling for user input
+            try
+            {
+                Console.WriteLine("Please provide your guessed number: ");
+                userGuess = int.Parse(Console.ReadLine());
+
+                //Setting range for user input.
+                if (userGuess < 1 || userGuess > 10000)
+                {
+                    Console.WriteLine("Error: Guess out of range, please provide correct number");
+                }
+                else if (userGuess == computerChoice)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You have guessed Computer number!!! Thank you for playing!");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (userGuess < computerChoice)
+                {
+                    Console.WriteLine("Too low.");
+                }
+                else if (userGuess > computerChoice)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Invalid user response. Please enter number you are trying to guess.");
+                }
+            }
+            //Catching Possible errors when parsing input.
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Parsing failed. Input is not a valid integer.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Error: Parsing failed. Input is null.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: An unexpected error occurred: " + ex.Message);
+            }
+        //Keeping loop going until user guess computer number
+        } while (userGuess != computerChoice);
+    }
 }
